3bca14ca7fd9fed9f57abdf7d80ce8b76022bd81
wangxuwei84 <wangxuewei84@gmail.com>
2011-01-04 16:20:03 +0800

    . #737 cleanup codes for dataProviderType when login and logout

--- js/ctd-daos-gdata.js
+++ js/ctd-daos-gdata.js
@@ -3,4 +3,35 @@ snow.dm.registerDao("contact",new GoogleContactDao());
 
 snow.dm.registerDao("group",new GoogleGroupDao());
 
-var dataProviderType = "googleData";
+var ctd = ctd || {};
+ctd.daos = ctd.daos || {};
+
+ctd.daos.dataProviderType = "googleData";
+
+ctd.daos.hasToken = function(){
+	return chrome.extension.getBackgroundPage().oauth.hasToken();
+}
+
+ctd.daos.flushData = function(){
+	chrome.extension.getBackgroundPage().getData(function(){
+		snow.ui.display("group");
+		snow.ui.display("contact");
+		snow.ui.display("welcome");
+	});
+}
+
+ctd.daos.getToken = function(callback){
+	var _callback = function(){
+		snow.ui.display("group");
+		snow.ui.display("contact");
+		snow.ui.display("welcome");
+		callback();
+	}
+	return chrome.extension.getBackgroundPage().getToken(function(){
+		_callback();
+	});
+}
+
+ctd.daos.logOut = function(){
+	chrome.extension.getBackgroundPage().logout();
+}
--- js/ctd-daos-mock.js
+++ js/ctd-daos-mock.js
@@ -3,4 +3,33 @@ snow.dm.registerDao("contact",new MockContactDao());
 
 snow.dm.registerDao("group",new MockGroupDao());
 
-var dataProviderType = "mockData";
+var ctd = ctd || {};
+ctd.daos = ctd.daos || {};
+
+ctd.daos.dataProviderType = "mockData";
+
+ctd.daos.hasToken = function(){
+	var hasToken = localStorage.getItem("oauth_tokenMock") ? true : false;
+	return hasToken;
+}
+
+ctd.daos.flushData = function(){
+	localStorage.setItem("groups",JSON.stringify(mockGroupDatas));
+	localStorage.setItem("contacts",JSON.stringify(mockContactDatas));
+	snow.ui.display("group");
+	snow.ui.display("contact");
+	snow.ui.display("welcome");
+}
+
+ctd.daos.getToken = function(callback){
+	localStorage.setItem("oauth_tokenMock","mockToken");
+	callback();
+	$logoffButton.show();
+	$loginButton.hide();
+	var token = localStorage.getItem("oauth_tokenMock");
+	return token;
+}
+
+ctd.daos.logOut = function(){
+	localStorage.removeItem("oauth_tokenMock");
+}
